5d3174bb69661c5d461fa6d75523c9eed432d6a4
removedporn <86824510+removedporn@users.noreply.github.com>
2022-02-04 23:20:59 +0800

    Delete case_studies directory

--- case_studies/pos_tagging/scripts/download_static_vectors.sh
+++
@@ -1,6 +0,0 @@
-# Download static word2vec embeddings
-wget --no-check-certificate --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1zhpyg5vxMT0iSMl7iW7KLW2wZ7phZFKE' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1zhpyg5vxMT0iSMl7iW7KLW2wZ7phZFKE" -O latin.200.vectors.txt && rm -f /tmp/cookies.txt
-mv latin.200.vectors.txt ../../models/
-
-
-
