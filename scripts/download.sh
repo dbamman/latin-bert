f1530fc2bc152549fcebeb83a0f360b345d7f8fa
removedporn <86824510+removedporn@users.noreply.github.com>
2022-02-04 23:22:11 +0800

    Delete scripts directory

--- scripts/download.sh
+++
@@ -1,9 +0,0 @@
-# Download BERT
-wget --no-check-certificate --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1Te_14UB-DZ8wYPhHGyDg7LadDTjNzpti' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1Te_14UB-DZ8wYPhHGyDg7LadDTjNzpti" -O latin_bert.tar && rm -f /tmp/cookies.txt
-mv latin_bert.tar models/
-cd models
-tar -xf latin_bert.tar
-rm latin_bert.tar
-
-
-
